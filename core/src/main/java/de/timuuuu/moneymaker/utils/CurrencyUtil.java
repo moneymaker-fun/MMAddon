0b6084589c5b2ed08bfaabde304bb0e22d63d315
MisterCore <Heiko-YouTube@web.de>
2023-10-09 22:25:57 +0200

    Added CurrencyUtil

---
+++ core/src/main/java/de/timuuuu/moneymaker/utils/CurrencyUtil.java
@@ -0,0 +1,67 @@
+package de.timuuuu.moneymaker.utils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CurrencyUtil {
+
+  public static Map<String, Integer> units = new HashMap<>();
+  public static void setUnits() {
+    units.put("K", 1);
+    units.put("M", 2);
+    units.put("B", 3);
+    units.put("T", 4);
+    units.put("aa", 5);
+    units.put("ab", 6);
+    units.put("ac", 7);
+    units.put("ad", 8);
+    units.put("ae", 9);
+    units.put("af", 10);
+    units.put("ag", 11);
+    units.put("ah", 12);
+    units.put("ai", 13);
+    units.put("aj", 14);
+    units.put("ak", 15);
+    units.put("al", 16);
+    units.put("am", 17);
+    units.put("an", 18);
+    units.put("ao", 19);
+    units.put("ap", 20);
+    units.put("aq", 21);
+    units.put("ar", 22);
+    units.put("as", 23);
+    units.put("at", 24);
+    units.put("au", 25);
+    units.put("av", 26);
+    units.put("aw", 27);
+    units.put("ax", 28);
+    units.put("ay", 29);
+    units.put("az", 30);
+    units.put("ba", 31);
+    units.put("bb", 32);
+    units.put("bc", 33);
+    units.put("bd", 34);
+    units.put("be", 35);
+    units.put("bf", 36);
+    units.put("bg", 37);
+    units.put("bh", 38);
+    units.put("bi", 39);
+    units.put("bj", 40);
+    units.put("bk", 41);
+    units.put("bl", 42);
+    units.put("bm", 43);
+    units.put("bn", 44);
+    units.put("bo", 45);
+    units.put("bp", 46);
+    units.put("bq", 47);
+    units.put("br", 48);
+    units.put("bs", 49);
+    units.put("bt", 50);
+    units.put("bu", 51);
+    units.put("bv", 52);
+    units.put("bw", 53);
+    units.put("bx", 54);
+    units.put("by", 55);
+    units.put("bz", 56);
+    }
+  }
