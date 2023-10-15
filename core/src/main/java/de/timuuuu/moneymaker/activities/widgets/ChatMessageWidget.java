12f495c7bf5a7ec0d78c9e5c11d2fd88c34f2c20
MisterCore <Heiko-YouTube@web.de>
2023-10-15 22:27:18 +0200

    WIP - Try new Chat

--- core/src/main/java/de/timuuuu/moneymaker/activities/widgets/ChatMessageWidget.java
+++ core/src/main/java/de/timuuuu/moneymaker/activities/widgets/ChatMessageWidget.java
@@ -1,18 +1,19 @@
 package de.timuuuu.moneymaker.activities.widgets;
 
 import de.timuuuu.moneymaker.utils.MoneyChatMessage;
-import de.timuuuu.moneymaker.utils.Util;
+import de.timuuuu.moneymaker.utils.MoneyPlayer.Rank;
 import net.labymod.api.client.component.Component;
 import net.labymod.api.client.gui.icon.Icon;
 import net.labymod.api.client.gui.screen.Parent;
 import net.labymod.api.client.gui.screen.widget.widgets.ComponentWidget;
 import net.labymod.api.client.gui.screen.widget.widgets.layout.list.HorizontalListWidget;
 
-public class ChatMessageWidget extends HorizontalListWidget {
+public class ChatMessageWidget extends HorizontalListWidget { // FlexibleContentWidget
 
   private String time;
   private MoneyChatMessage chatMessage = null;
   private String customMessage;
+  private boolean headless = false;
 
   public ChatMessageWidget(String time, MoneyChatMessage chatMessage) {
     this.time = time;
@@ -22,8 +23,42 @@ public class ChatMessageWidget extends HorizontalListWidget {
   public ChatMessageWidget(String time, String customMessage) {
     this.time = time;
     this.customMessage = customMessage;
+    this.headless = true;
   }
 
+  /*@Override
+  public void initialize(Parent parent) {
+    super.initialize(parent);
+
+    VerticalListWidget<Widget> flex = new VerticalListWidget<>().addId("flex");
+
+    if(!this.headless) {
+      HorizontalListWidget header = new HorizontalListWidget().addId("message-header");
+      String color;
+      if(!Util.isDev(chatMessage.uuid().toString())) {
+        color = chatMessage.staff() ? "§8[§cStaff§8] §c" : "§b";
+      } else {
+        color = "§8[§4Dev§8] §c";
+      }
+      header.addEntry(ComponentWidget.text(color + chatMessage.userName()).addId("sender"));
+      header.addEntry(ComponentWidget.text(time).addId("timestamp"));
+      flex.addChild(header);
+    }
+
+    VerticalListWidget<Widget> messageContentWidget = new VerticalListWidget<>().addId("message-content");
+    ComponentWidget componentMessageWidget = ComponentWidget.text(chatMessage != null ? chatMessage.message() : customMessage).addId(new String[] {"component-message", "tile"});
+    messageContentWidget.addChild(componentMessageWidget);
+    if(!this.headless) {
+      messageContentWidget.addId("headless");
+    }
+
+    this.addContent(flex);
+    if(!this.headless) {
+      IconWidget avatar = new IconWidget(Icon.head(chatMessage.uuid())).addId("avatar");
+      this.addContent(avatar);
+    }
+
+  }*/
 
   @Override
   public void initialize(Parent parent) {
@@ -31,16 +66,21 @@ public class ChatMessageWidget extends HorizontalListWidget {
 
     if(this.chatMessage != null) {
 
-      String color;
-      if(!Util.isDev(chatMessage.uuid().toString())) {
-        color = chatMessage.staff() ? "§8[§cStaff§8] §c" : "§b";
+      String prefix;
+      if(chatMessage.rank() == Rank.DEVELOPER) {
+        prefix = "§8[§4Dev§8] §4";
+      } else if(chatMessage.rank() == Rank.STAFF) {
+        prefix = "§8[§cStaff§8] §c";
+      } else if(chatMessage.rank() == Rank.DONATOR) {
+        prefix = "§8[§6Don§8] §6";
       } else {
-        color = "§8[§4Dev§8] §c";
+        prefix = "§e";
       }
 
       this.addEntry(ComponentWidget.text("§e" + time + " ").addId("chat-msg-time"));
-      this.addEntry(ComponentWidget.component(Component.icon(Icon.head(this.chatMessage.uuid(), true, false), 10)).addId("chat-msg-icon"));
-      this.addEntry(ComponentWidget.text(" " + color + chatMessage.userName() + " §8- §7").addId("chat-msg-playerName"));
+      this.addEntry(ComponentWidget.component(
+          Component.icon(Icon.head(this.chatMessage.uuid(), true, false), 10)).addId("chat-msg-icon"));
+      this.addEntry(ComponentWidget.text(" " + prefix + chatMessage.userName() + " §8- §7").addId("chat-msg-playerName"));
       //this.addEntry(ComponentWidget.text(chatMessage.message()).addId("chat-msg-message"));
 
       // Split: 65; 130; 195;
