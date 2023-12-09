82b14af7a8a77b8bcc2e7ba41fd21e26f4f01919
MisterCore <Heiko-YouTube@web.de>
2023-12-09 15:34:42 +0100

    Added support for 1.20.4

---
+++ game-runner/src/v1_20_4/java/de/timuuuu/moneymaker/v1_20_4/mixins/SwordTickMixin.java
@@ -0,0 +1,47 @@
+package de.timuuuu.moneymaker.v1_20_4.mixins;
+
+import de.timuuuu.moneymaker.event.SwordTickEvent;
+import java.util.ArrayList;
+import java.util.List;
+import net.labymod.api.Laby;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.ListTag;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Minecraft.class)
+public class SwordTickMixin {
+
+  @Shadow
+  public @Nullable LocalPlayer player;
+
+  @Inject(
+      method = {"tick()V"},
+      at = @At("HEAD")
+  )
+  private void moneymaker$tick(CallbackInfo ci) {
+    Player player = Minecraft.getInstance().player;
+    if(player != null) {
+      ItemStack itemStack = player.getInventory().getItem(0);
+      if(itemStack != ItemStack.EMPTY) {
+        CompoundTag compoundTag = itemStack.getOrCreateTagElement("display");
+        String name = compoundTag.getString(ItemStack.TAG_DISPLAY_NAME);
+        List<String> loreList = new ArrayList<>();
+        ListTag listTag = compoundTag.getList(ItemStack.TAG_LORE, 8);
+        for(int i = 0; i != listTag.size(); i++) {
+          loreList.add(listTag.getString(i));
+        }
+        Laby.fireEvent(new SwordTickEvent(name, loreList, "1.20"));
+      }
+    }
+  }
+
+}
