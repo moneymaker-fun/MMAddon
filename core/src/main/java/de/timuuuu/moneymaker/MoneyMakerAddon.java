39600a263d2dbc81050c5837d88ac7f5b50577e8
MisterCore <Heiko-YouTube@web.de>
2023-12-30 14:30:16 +0100

    Bump to 1.1.0

--- core/src/main/java/de/timuuuu/moneymaker/MoneyMakerAddon.java
+++ core/src/main/java/de/timuuuu/moneymaker/MoneyMakerAddon.java
@@ -69,7 +69,7 @@ public class MoneyMakerAddon extends LabyAddon<MoneyMakerConfiguration> {
 
   @Override
   protected void preConfigurationLoad() {
-    Laby.references().revisionRegistry().register(new SimpleRevision("moneymaker", new SemanticVersion("1.0.0"), "2023-12-16"));
+    Laby.references().revisionRegistry().register(new SimpleRevision("moneymaker", new SemanticVersion("1.1.0"), "2023-12-30"));
   }
 
   @Override
