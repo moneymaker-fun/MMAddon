080bd34e6d0b983c5878c6768891402c8808bdea
MisterCore <Heiko-YouTube@web.de>
2024-02-10 12:31:46 +0100

    Added new Setting 'Load Chat History'

--- core/src/main/java/de/timuuuu/moneymaker/settings/MoneyChatConfiguration.java
+++ core/src/main/java/de/timuuuu/moneymaker/settings/MoneyChatConfiguration.java
@@ -2,13 +2,24 @@ package de.timuuuu.moneymaker.settings;
 
 import net.labymod.api.client.gui.screen.widget.widgets.input.SwitchWidget.SwitchSetting;
 import net.labymod.api.configuration.loader.Config;
+import net.labymod.api.configuration.loader.annotation.IntroducedIn;
 import net.labymod.api.configuration.loader.annotation.SpriteSlot;
 import net.labymod.api.configuration.loader.annotation.SpriteTexture;
 import net.labymod.api.configuration.loader.property.ConfigProperty;
 import net.labymod.api.configuration.settings.annotation.SettingRequires;
+import net.labymod.api.configuration.settings.annotation.SettingSection;
 
 @SpriteTexture("sprite/settings")
 public class MoneyChatConfiguration extends Config {
+
+  @SettingSection(value = "general", center = true)
+
+  @IntroducedIn(namespace = "moneymaker", value = "1.3.1")
+  @SpriteSlot(x = 1)
+  @SwitchSetting
+  private final ConfigProperty<Boolean> loadChatHistory = new ConfigProperty<>(true);
+
+  @SettingSection(value = "notifications", center = true)
 
   @SpriteSlot(y = 4)
   @SwitchSetting
@@ -22,6 +33,14 @@ public class MoneyChatConfiguration extends Config {
   @SpriteSlot(y = 4, x = 2)
   @SwitchSetting
   private final ConfigProperty<Boolean> onlineOfflineMessages = new ConfigProperty<>(true);
+
+  // General Getters
+
+  public ConfigProperty<Boolean> loadChatHistory() {
+    return loadChatHistory;
+  }
+
+  // Notification Getters
 
   public ConfigProperty<Boolean> notification() {
     return notification;
