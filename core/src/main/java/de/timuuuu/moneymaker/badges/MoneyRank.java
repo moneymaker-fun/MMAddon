77dc886a86e8d988ee1cf060e8e4309064601a65
MisterCore <Heiko-YouTube@web.de>
2024-03-15 21:33:04 +0100

    Added MoneyRank#iconUrl

--- core/src/main/java/de/timuuuu/moneymaker/badges/MoneyRank.java
+++ core/src/main/java/de/timuuuu/moneymaker/badges/MoneyRank.java
@@ -7,10 +7,10 @@ import net.labymod.api.client.gui.icon.Icon;
 
 public enum MoneyRank {
 
-  USER(4, "user", false, "§e", "§e", null, Common.ICON),
-  DONATOR(3, "donator", false, "§8[§6$§8] §6", "§6", Component.text(" Donator", TextColor.color(255, 170, 0)), Common.ICON_ORANGE),
-  STAFF(2, "staff", true, "§8[§cStaff§8] §c", "§c", Component.text(" Staff", TextColor.color(255, 85, 85)), Common.ICON_RED),
-  DEVELOPER(1, "developer", true, "§8[§4Dev§8] §4", "§4", Component.text(" Dev", TextColor.color(170, 0, 0)), Common.ICON_DARK_RED);
+  USER(4, "user", false, "§e", "§e", null, Common.ICON, "https://cdn.terramc.net/moneymaker/lore.png"),
+  DONATOR(3, "donator", false, "§8[§6$§8] §6", "§6", Component.text(" Donator", TextColor.color(255, 170, 0)), Common.ICON_ORANGE, "https://cdn.terramc.net/moneymaker/lore_orange.png"),
+  STAFF(2, "staff", true, "§8[§cStaff§8] §c", "§c", Component.text(" Staff", TextColor.color(255, 85, 85)), Common.ICON_RED, "https://cdn.terramc.net/moneymaker/lore_light_red.png"),
+  DEVELOPER(1, "developer", true, "§8[§4Dev§8] §4", "§4", Component.text(" Dev", TextColor.color(170, 0, 0)), Common.ICON_DARK_RED, "https://cdn.terramc.net/moneymaker/lore_red.png");
 
   private final int id;
   private final String name;
@@ -19,8 +19,9 @@ public enum MoneyRank {
   private final String onlineColor;
   private final Component nameTag;
   private final Icon icon;
+  private final String iconUrl;
 
-  MoneyRank(int id, String name, boolean staff, String chatPrefix, String onlineColor, Component nameTag, Icon icon) {
+  MoneyRank(int id, String name, boolean staff, String chatPrefix, String onlineColor, Component nameTag, Icon icon, String iconUrl) {
     this.id = id;
     this.name = name;
     this.staff = staff;
@@ -28,6 +29,7 @@ public enum MoneyRank {
     this.onlineColor = onlineColor;
     this.nameTag = nameTag;
     this.icon = icon;
+    this.iconUrl = iconUrl;
   }
 
   public int getId() {
@@ -56,5 +58,9 @@ public enum MoneyRank {
 
   public Icon getIcon() {
     return icon;
+  }
+
+  public String getIconUrl() {
+    return iconUrl;
   }
 }
