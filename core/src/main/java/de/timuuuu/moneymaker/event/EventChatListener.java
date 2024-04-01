1934016ff9869ec3a1d3a9f73c2c094000b7601c
MisterCore <Heiko-YouTube@web.de>
2024-04-01 14:45:08 +0200

    Added ValentineEventWidget

--- core/src/main/java/de/timuuuu/moneymaker/event/EventChatListener.java
+++ core/src/main/java/de/timuuuu/moneymaker/event/EventChatListener.java
@@ -2,6 +2,7 @@ package de.timuuuu.moneymaker.event;
 
 import de.timuuuu.moneymaker.MoneyMakerAddon;
 import de.timuuuu.moneymaker.event.hudwidget.EasterEventWidget;
+import de.timuuuu.moneymaker.event.hudwidget.ValentineEventWidget;
 import net.labymod.api.event.Priority;
 import net.labymod.api.event.Subscribe;
 import net.labymod.api.event.client.chat.ChatReceiveEvent;
@@ -31,9 +32,19 @@ public class EventChatListener {
         } catch (NumberFormatException ignored) {}
       }
 
+      if(EventChatMessages.VALENTINE_DE.contains(plain) || EventChatMessages.VALENTINE_EN.contains(plain)) {
+        String count = plain.replace(EventChatMessages.NOW_DE.message(), "")
+            .replace(EventChatMessages.VALENTINE_DE.message(), "")
+            .replace(EventChatMessages.NOW_EN.message(), "")
+            .replace(EventChatMessages.VALENTINE_EN.message(), "");
+        try {
+          ValentineEventWidget.flowers = Integer.parseInt(count);
+        } catch (NumberFormatException ignored) {}
+      }
+
     }
 
-    if((EventChatMessages.BOOSTER_DE.contains(plain) || EventChatMessages.BOOSTER_EN.contains(plain))) {
+    if(EventChatMessages.BOOSTER_DE.contains(plain) || EventChatMessages.BOOSTER_EN.contains(plain)) {
       if(this.addon.addonUtil().currentEvent().equals("EASTER")) {
         if(EasterEventWidget.eggs >= 5) {
           EasterEventWidget.eggs -=5;
