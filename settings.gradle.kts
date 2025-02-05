36242e0b6e100db59784285b22fa841608d8f97c
MisterCore <Heiko-YouTube@web.de>
2025-02-05 18:14:53 +0100

    Change LabyGradle Version to 0.5.8

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 rootProject.name = "moneymaker"
 
 pluginManagement {
-    val labyGradlePluginVersion = "0.5.7"
+    val labyGradlePluginVersion = "0.5.8"
     buildscript {
         repositories {
             maven("https://dist.labymod.net/api/v1/maven/release/")
