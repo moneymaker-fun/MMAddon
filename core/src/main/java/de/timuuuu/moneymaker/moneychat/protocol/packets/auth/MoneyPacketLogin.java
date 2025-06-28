fa0b820a5ad63303977d1e3f60ed5789657d70b7
MisterCore <Heiko-YouTube@web.de>
2025-06-28 18:29:58 +0200

    Added Login Packet

---
+++ core/src/main/java/de/timuuuu/moneymaker/moneychat/protocol/packets/auth/MoneyPacketLogin.java
@@ -0,0 +1,33 @@
+package de.timuuuu.moneymaker.moneychat.protocol.packets.auth;
+
+import de.timuuuu.moneymaker.moneychat.protocol.MoneyPacket;
+import de.timuuuu.moneymaker.moneychat.protocol.MoneyPacketBuffer;
+import de.timuuuu.moneymaker.moneychat.protocol.MoneyPacketHandler;
+import java.util.UUID;
+
+public class MoneyPacketLogin extends MoneyPacket {
+
+  private String username;
+  private UUID uuid;
+
+  public MoneyPacketLogin(String username, UUID uuid) {
+    this.username = username;
+    this.uuid = uuid;
+  }
+
+  @Override
+  public void read(MoneyPacketBuffer packetBuffer) {
+    username = packetBuffer.readString();
+    uuid = packetBuffer.readUUID();
+  }
+
+  @Override
+  public void write(MoneyPacketBuffer packetBuffer) {
+    packetBuffer.writeString(username);
+    packetBuffer.writeUUID(uuid);
+  }
+
+  @Override
+  public void handle(MoneyPacketHandler packet) {}
+
+}
