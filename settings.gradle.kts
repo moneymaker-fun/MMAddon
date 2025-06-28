d5a8f55e5bb46d89f611342ed57f82c757014ced
MisterCore <Heiko-YouTube@web.de>
2025-06-28 15:52:06 +0200

    Removed dep from settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,7 +13,6 @@ pluginManagement {
 
         dependencies {
             classpath("net.labymod.gradle", "common", labyGradlePluginVersion)
-            classpath("io.netty", "netty-all", "4.1.97.Final")
         }
     }
 }
