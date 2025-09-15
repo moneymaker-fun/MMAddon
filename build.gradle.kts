661adce82a69c8c11af6d12de9e9a62e221110ed
MisterCore <Heiko-YouTube@web.de>
2025-09-15 18:13:39 +0200

    Bumped addon version to `1.7.0` in build configuration.

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     id("net.labymod.labygradle.addon")
 }
 
-val addonVersion = "1.6.7"
+val addonVersion = "1.7.0"
 
 val versions = providers.gradleProperty("net.labymod.minecraft-versions").get().split(";")
 
