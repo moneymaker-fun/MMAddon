a5e8cea930858e396c257c694ccb08054bf60733
MisterCore <Heiko-YouTube@web.de>
2025-09-25 17:01:46 +0200

    Switched `netty-all` dependency to `compileOnly` in build configuration.

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -10,7 +10,8 @@ dependencies {
 
     // An example of how to add an external dependency that is used by the addon.
     // addonMavenDependency("org.jeasy:easy-random:5.0.0")
-    addonMavenDependency("io.netty:netty-all:4.1.97.Final")
+    //addonMavenDependency("io.netty:netty-all:4.1.97.Final")
+    compileOnly("io.netty:netty-all:4.1.97.Final")
 }
 
 labyModAnnotationProcessor {
