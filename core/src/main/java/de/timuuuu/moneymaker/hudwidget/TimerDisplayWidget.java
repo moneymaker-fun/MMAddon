9c3d86afb73167e363dd153701b4bd94eca26383
MisterCore <Heiko-YouTube@web.de>
2025-12-19 21:30:29 +0100

    Refactored HUD widgets to use `RenderPhase` and `ScreenContext` for rendering, replaced deprecated `Stack` usage with `TextRenderingOptions` and `Canvas` API.

--- core/src/main/java/de/timuuuu/moneymaker/hudwidget/TimerDisplayWidget.java
+++ core/src/main/java/de/timuuuu/moneymaker/hudwidget/TimerDisplayWidget.java
@@ -9,6 +9,7 @@ import java.util.List;
 import net.labymod.api.client.component.Component;
 import net.labymod.api.client.component.format.NamedTextColor;
 import net.labymod.api.client.component.format.TextColor;
+import net.labymod.api.client.gfx.pipeline.renderer.text.TextRenderingOptions;
 import net.labymod.api.client.gui.hud.hudwidget.HudWidgetConfig;
 import net.labymod.api.client.gui.hud.hudwidget.SimpleHudWidget;
 import net.labymod.api.client.gui.hud.position.HudSize;
@@ -16,9 +17,7 @@ import net.labymod.api.client.gui.icon.Icon;
 import net.labymod.api.client.gui.screen.ScreenContext;
 import net.labymod.api.client.gui.screen.widget.widgets.input.SliderWidget.SliderSetting;
 import net.labymod.api.client.gui.screen.widget.widgets.input.color.ColorPickerWidget.ColorPickerSetting;
-import net.labymod.api.client.render.font.ComponentRenderer;
 import net.labymod.api.client.render.font.RenderableComponent;
-import net.labymod.api.client.render.matrix.Stack;
 import net.labymod.api.client.resources.ResourceLocation;
 import net.labymod.api.configuration.loader.property.ConfigProperty;
 import net.labymod.api.util.Color;
@@ -41,58 +40,63 @@ public class TimerDisplayWidget extends SimpleHudWidget<TimerHudWidgetConfig> {
   }
 
   @Override
-  public void render(RenderPhase renderPhase, ScreenContext context, boolean isEditorContext, HudSize size) {
-    size.setHeight(0F);
-    size.setWidth(0F);
+  public void render(RenderPhase renderPhase, ScreenContext screenContext, boolean isEditorContext, HudSize size) {
+    size.setHeight(0.0F);
+    size.setWidth(0.0F);
 
     if(isEditorContext) {
-      this.renderTimers(this.dummyTimers, context.stack(), size);
+      this.renderTimers(this.dummyTimers, renderPhase, screenContext, size);
       return;
     }
 
     if(!this.addon.addonUtil().connectedToMoneyMaker()) {
-      this.renderComponent(Component.translatable("moneymaker.hudWidget.mm_timer_display.notConnected"), context.stack(), size);
+      this.renderComponent(Component.translatable("moneymaker.hudWidget.mm_timer_display.notConnected"), renderPhase, screenContext, size);
       return;
     }
 
     List<MoneyTimer> timers = new ArrayList<>(Util.timers.values());
     if(timers.isEmpty()) {
-      this.renderComponent(Component.translatable("moneymaker.hudWidget.mm_timer_display.noTimers"), context.stack(), size);
+      this.renderComponent(Component.translatable("moneymaker.hudWidget.mm_timer_display.noTimers"), renderPhase, screenContext, size);
       return;
     }
 
-    this.renderTimers(timers, context.stack(), size);
+    this.renderTimers(timers, renderPhase, screenContext, size);
   }
 
-  private void renderComponent(Component component, Stack stack, HudSize size) {
+  private void renderComponent(Component component, RenderPhase phase, ScreenContext screenContext, HudSize size) {
     RenderableComponent renderableComponent = RenderableComponent.of(component);
-    if(stack != null) {
-      this.labyAPI.renderPipeline().componentRenderer().builder()
-          .text(renderableComponent)
-          .pos(1, 1)
-          .render(stack);
+    if(phase.canRender()) {
+      screenContext.canvas().submitRenderableComponent(
+          renderableComponent,
+          1,
+          1,
+          -1,
+          TextRenderingOptions.SHADOW
+      );
     }
     size.setWidth(renderableComponent.getWidth() +2);
     size.setHeight(renderableComponent.getHeight() +2);
   }
 
-  private void renderTimers(List<MoneyTimer> timers, Stack stack, HudSize size) {
-    ComponentRenderer componentRenderer = this.labyAPI.renderPipeline().componentRenderer();
+  private void renderTimers(List<MoneyTimer> timers, RenderPhase phase, ScreenContext screenContext, HudSize size) {
     int x = 1;
     int y = 1;
     Component title = Component.translatable("moneymaker.hudWidget.mm_timer_display.currentTimers", TextColor.color(this.config.textColor.get().get()));
     RenderableComponent titleComponent = RenderableComponent.of(title);
-    if(stack != null) {
-      componentRenderer.builder()
-          .text(titleComponent)
-          .pos(x, y)
-          .render(stack);
+    if(phase.canRender()) {
+      screenContext.canvas().submitRenderableComponent(
+          titleComponent,
+          x,
+          y,
+          -1,
+          TextRenderingOptions.SHADOW
+      );
     }
 
     size.setWidth(x + titleComponent.getWidth() + 1);
     //x += 1;
     y += (int) (titleComponent.getHeight() +1);
-    int rowHeight = (int) componentRenderer.height();
+    int rowHeight = (int) screenContext.canvas().getLineHeight();
 
     int maxTimerIndex = this.config.maxDisplayedTimers.get() -1;
     int timerIndex = 0;
@@ -100,10 +104,12 @@ public class TimerDisplayWidget extends SimpleHudWidget<TimerHudWidgetConfig> {
       if(timerIndex > maxTimerIndex) break;
       timerIndex++;
       int timerX = x; //int timerX = x + 2;
-      if(stack != null) {
+      if(phase.canRender()) {
         Icon icon = Icon.sprite16(
             ResourceLocation.create("moneymaker", "themes/vanilla/textures/settings/hud/hud.png"), 0, 2);
-        icon.render(stack, timerX, y, rowHeight);
+        screenContext.canvas().submitIcon(
+            icon, timerX, y, rowHeight, rowHeight
+        );
       }
 
       timerX += rowHeight +4;
@@ -113,11 +119,14 @@ public class TimerDisplayWidget extends SimpleHudWidget<TimerHudWidgetConfig> {
           .append(Component.text("] » ", NamedTextColor.DARK_GRAY))
           .append(Component.text(timer.remainingTime(), NamedTextColor.YELLOW));
       RenderableComponent timerName = RenderableComponent.of(component);
-      if(stack != null) {
-        componentRenderer.builder()
-            .text(timerName)
-            .pos(timerX, y)
-            .render(stack);
+      if(phase.canRender()) {
+        screenContext.canvas().submitRenderableComponent(
+            timerName,
+            timerX,
+            y,
+            -1,
+            TextRenderingOptions.SHADOW
+        );
       }
 
       y += (int) (timerName.getHeight() +1);
